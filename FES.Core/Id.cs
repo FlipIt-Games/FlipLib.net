6df86aa73d4ed7a4530283268d7bc14c8795a177
Sylvain Delépée <sylvain.delepee@gmail.com>
2025-02-26 12:29:15 +0100

    add equals operator to Id

--- FES.Core/Id.cs
+++ FES.Core/Id.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace FES;
 
 public readonly struct Id<T> where T : struct 
@@ -11,4 +13,10 @@ public readonly struct Id<T> where T : struct
 
     public static explicit operator int (Id<T> id) => id.Value;
     public static explicit operator Id<T>(int id) => new Id<T>(id);
+
+    public static bool operator ==(Id<T> id, Id<T>other) => id.Value == other.Value;
+    public static bool operator !=(Id<T> id, Id<T>other) => id.Value != other.Value;
+
+    public override bool Equals([NotNullWhen(true)] object obj) => Value.Equals(obj);
+    public override int GetHashCode() => Value.GetHashCode();
 }
