89090cd1f748482ce03b70345adc960d0e428284
Sylvain Delépée <sylvain.delepee@gmail.com>
2025-02-26 18:07:10 +0100

    test

--- FES.Core/Id.cs
+++ FES.Core/Id.cs
@@ -19,4 +19,9 @@ public readonly struct Id<T> where T : struct
 
     public override bool Equals([NotNullWhen(true)] object obj) => Value.Equals(obj);
     public override int GetHashCode() => Value.GetHashCode();
+
+    public void Test()
+    {
+        Console.WriteLine("Test");
+    }
 }
